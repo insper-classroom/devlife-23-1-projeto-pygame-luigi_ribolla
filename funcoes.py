da9f80803e17460e281b520e0f64153bf3b06d4a
Luigi Quinze <luigioq@al.insper.edu.br>
2023-04-20 16:36:27 -0300

    primeira versão mapa

--- funcoes.py
+++ funcoes.py
@@ -16,22 +16,15 @@ class Jogo:
         return True
     
     def desenha(self):
-        self.window.fill((100,100,100))
+        self.window.blit(self.mapa.map, (0,0))
         self.window.blit(self.player.img, self.player.rect)
-        for parede in self.mapa.paredes:
-            self.window.blit(self.mapa.wall, (parede))
+
         pg.display.flip()
 
 class Mapa:
     def __init__(self):
-        self.wall = pg.image.load("docs/assets/img/wall.png")
-        self.paredes = [
-            pg.Rect(0, 0, 20, 600),
-            pg.Rect(0, 0, 800, 20),
-            pg.Rect(780, 0, 20, 600),
-            pg.Rect(0, 580, 800, 20),
-            pg.Rect(200, 200, 400, 200) ]
-
+        img = pg.image.load("docs/assets/img/backg.png")
+        self.map = pg.transform.scale(img,(800,600))
 
 class Player:
     def __init__(self):
@@ -45,10 +38,11 @@ class Player:
         
         img = pg.image.load("docs/assets/img/hunter.png")
         self.img = pg.transform.scale(img, (250, 250))
-        self.rect = pg.Rect(375, 275, 250, 250)
+        self.rect = pg.Rect(350, 500, 250, 250)
         self.vel = [0,0]
 
     def movimento(self,event):
+        
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_a:
                 self.vel[0] = -3
@@ -60,13 +54,13 @@ class Player:
                 self.vel[1] = 3
 
         elif event.type == pg.KEYUP:
-            if event.key == pg.K_a and self.vel[0] < 0:
+            if event.key == pg.K_a:
                 self.vel[0] = 0
-            elif event.key == pg.K_d and self.vel[0] > 0:
+            elif event.key == pg.K_d:
                 self.vel[0] = 0
-            elif event.key == pg.K_w and self.vel[1] < 0:
+            elif event.key == pg.K_w:
                 self.vel[1] = 0
-            elif event.key == pg.K_s and self.vel[1] > 0:
+            elif event.key == pg.K_s:
                 self.vel[1] = 0
             
         self.rect.x += self.vel[0] 
