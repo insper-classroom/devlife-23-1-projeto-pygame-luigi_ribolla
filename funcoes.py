51c49620904a6d7365cf491034dd170dd2442f84
Luigi Quinze <luigioq@al.insper.edu.br>
2023-04-20 11:00:17 -0300

    Movimentaçao do jogador

--- funcoes.py
+++ funcoes.py
@@ -1,56 +1,12 @@
 import pygame as pg
 
-class Player:
-    def __init__(self):
-        self.sprites = {
-            'default ': pg.image.load("docs/assets/img/hunter.png"),
-            'move': pg.image.load("docs/assets/img/hunter_move_D.png"),
-            'power': [],
-            'attack': [],
-            'morte': [],
-        }
-        img = pg.image.load("docs/assets/img/hunter.png")
-        self.timer = pg.get_ticks()
-        self.player_img = pg.transform.scale(img, (250, 250))
-        self.player_hitbox = pg.get_Rect()
-        self.power_use = False
-        self.pos = [300, 190]
-        self.vel = 0.3
-
-    def movimento(self):
-         
-        tecla = pg.key.get_pressed()
-        if tecla[pg.K_a] or tecla[pg.K_LEFT]:
-            self.pos[0] -= self.vel
-        if tecla[pg.K_d] or tecla[pg.K_RIGHT]:
-            self.player_img = pg
-            self.pos[0] += self.vel
-        if tecla[pg.K_w] or tecla[pg.K_UP]:
-            self.pos[1] -= self.vel
-        if tecla[pg.K_s] or tecla[pg.K_DOWN]:
-            self.pos[1] += self.vel
-         
-        for event in pg.event.get():
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.SPACE:
-                    self.power()
-
-        def power(self):
-
-# class time:
-#     def __init__(self):
-#         self.frames = pg.get_ticks()
-        
-class jogo:
+class Jogo:
     def __init__(self):
         pg.init()
         largura, altura = 800, 600
         self.window = pg.display.set_mode((largura, altura))
         self.player = Player()
-
-        self.state = {
-        'mapa' : (2000,1500), # tamanho do mapa
-        }
+        self.mapa = Mapa()
 
     def checa_eventos(self):
         for event in pg.event.get():
@@ -61,10 +17,60 @@ class jogo:
     
     def desenha(self):
         self.window.fill((100,100,100))
-        self.window.blit(self.player.player_img, self.player.pos)
+        self.window.blit(self.player.img, self.player.rect)
+        for parede in self.mapa.paredes:
+            self.window.blit(self.mapa.wall, (parede))
         pg.display.flip()
 
-game = jogo()
+class Mapa:
+    def __init__(self):
+        self.wall = pg.image.load("docs/assets/img/wall.png")
+        self.paredes = [
+            pg.Rect(0, 0, 20, 600),
+            pg.Rect(0, 0, 800, 20),
+            pg.Rect(780, 0, 20, 600),
+            pg.Rect(0, 580, 800, 20),
+            pg.Rect(200, 200, 400, 200) ]
+
+
+class Player:
+    def __init__(self):
+        self.sprites = {
+            'default ': pg.image.load("docs/assets/img/hunter.png"),
+            'move': pg.image.load("docs/assets/img/hunter_move_D.png"),
+            'power': [],
+            'attack': [],
+            'morte': [],
+        }
+        img = pg.image.load("docs/assets/img/hunter.png")
+        self.img = pg.transform.scale(img, (250, 250))
+        self.rect = pg.Rect(375, 275, 250, 250)
+        self.vel = (0,0)
+
+    def movimento(self):
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_a:
+                    self.vel[0] = -0.5
+                elif event.key == pg.K_d:
+                    self.vel[0] = 0.5
+                elif event.key == pg.K_w:
+                    self.vel[1] = -0.5
+                elif event.key == pg.K_s:
+                    self.vel[1] = 0.5
+            elif event.type == pg.KEYUP:
+                if event.key == pg.K_a and self.vel[0] < 0:
+                    self.vel[0] = 0
+                elif event.key == pg.K_d and self.vel[0] > 0:
+                    self.vel[0] = 0
+                elif event.key == pg.K_w and self.vel[1] < 0:
+                    self.vel[1] = 0
+                elif event.key == pg.K_s and self.vel[1] > 0:
+                    self.vel[1] = 0
+            
+        self.rect.x += self.vel[0] 
+        self.rect.y += self.vel[1]
+game = Jogo()
 
 while game.checa_eventos():
     game.desenha()
